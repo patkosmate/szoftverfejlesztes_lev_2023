fc5fb0e8f075df71702c3b8d63c151bc48130074
patkosmate <patkosmateleventeuj@gmail.com>
2023-04-20 11:15:39 +0200

    elso commit

---
+++ src/main/java/org/example/FXMLStudentsSceneController.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class FXMLStudentsSceneController {
+    @FXML
+    private Label birthDayLabel;
+
+    @FXML
+    private Label creditsLabel;
+
+    @FXML
+    private Label helloLabel;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    void handleLoadButtonPushed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleButtonPushed(){
+        System.out.println("Hello world!!!");
+        if (helloLabel.getText().equals("fekete")) helloLabel.setText("fehér");
+        else helloLabel.setText("fekete");
+    }
+}
